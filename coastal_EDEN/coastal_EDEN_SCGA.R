f1c2bde2aeab7a2aaf11bd680ffd4aecb7b3b52b
Bryan McCloskey <bmccloskey@usgs.gov>
2019-05-01 12:47:03 -0400

    Temperature, stage plots added.

--- coastal_EDEN/coastal_EDEN_SCGA.R
+++ coastal_EDEN/coastal_EDEN_SCGA.R
@@ -33,6 +33,7 @@ for (j in c("salinity", "temperature", "stage")) {
     cd <- if (j == "salinity") "00095" else if (j == "temperature") "00010" else "00065"
     # Generate AQUARIUS URLs; add 1 for DST end timestamps
     url <- paste0("https://waterservices.usgs.gov/nwis/dv/?format=rdb&sites=", gages$NWIS_ID[i], "&startDT=", days[1], "&endDT=", days[2], "&parameterCd=", cd, "&statCd=00003&access=3")
+    print(url)
     tmp <- try (read.table(url, header = T, sep = "\t", colClasses = "character"))
     if (inherits(tmp, "try-error")) {
       report <- paste0(report, j, " data _NOT_ downloaded for ", gages$NWIS_ID[i], "\n")
@@ -75,9 +76,9 @@ for (j in c("salinity", "temperature", "stage")) {
     date_check <- dbGetQuery(con, paste0("select count(date) as ct from coastal_sc_ga where date = '", range[i], "'"))
     in_up <- if (date_check$ct == 1) "update" else "insert into"
     query <- paste0(in_up, " coastal_sc_ga set date='", v$datetime[i], "'")
-    for (j in 2:dim(v)[2]) {
-      if (is.na(v[i, j])) tmp <- "NULL" else tmp <- v[i, j]
-      query <- paste0(query, ", ", names(v)[j], " = ", tmp)
+    for (k in 2:dim(v)[2]) {
+      if (is.na(v[i, k])) tmp <- "NULL" else tmp <- v[i, k]
+      query <- paste0(query, ", ", names(v)[k], " = ", tmp)
     }
     if (date_check$ct == 1)
       query <- paste0(query, " where date = '", range[i], "'")
@@ -85,42 +86,45 @@ for (j in c("salinity", "temperature", "stage")) {
     err <- try(dbSendQuery(con, query), T)
     if (inherits(err, "try-error")) report <- paste0(report, "\nCoastalEDENdb upload error for ", v$datetime[i], ": ", err)
   } # End process and upload
-}
 
-# Create year+ plots of parameters, and thumbnails
-for (i in 1:dim(gages)[1]) {
-  db2 <- dbGetQuery(con, paste0("select date, ", gages$NWIS_ID[i], "_salinity as sal from coastal_sc_ga where date >= ", as.numeric(format(Sys.Date(), "%Y")) - 3, format(Sys.Date(), "%m"), "01 order by date"))
-  db2$date <- as.Date(db2$date, format = "%Y-%m-%d")
-  last_meas <- rev(which(!is.na(db2$sal)))[1]
-  db3 <- data.frame(date = db2$date, sal = NA)
-  for (j in 7:last_meas) db3$sal[j] <- mean(db2$sal[(j - 6):j], na.rm = T)
-  jpeg(paste0("./images/", gages$NWIS_ID[i], "thumb.jpg"), width = 360, height = 150, pointsize = 2, quality = 100, type = "quartz")
-  par(mar = c(0, 0, 0, 0) + .1)
-  plot(db2$date, db2$sal, type = "n", xlab = "", ylab = "", ylim = range(db2$sal, na.rm = T), xaxt = "n", xaxt = "n", main = "")
-  grid(nx = NA, ny = NULL, col = "black", lty = "dashed")
-  abline(v = which(format(db2$date, "%m") == "01" & format(db2$date, "%d") == "01"), lty = "dashed", col = "black")
-  lines(db2$date, db2$sal, lwd = 2)
-  points(db2$date[last_meas], db2$sal[last_meas], pch = 20)
-  lines(db3$date, db3$sal, lwd = 2, col = "green")
-  points(db3$date[last_meas], db3$sal[last_meas - 6], pch = 20, col = "green")
-  dev.off()
-  jpeg(paste0("./images/", gages$NWIS_ID[i], ".jpg"), width = 2400, height = 800, quality = 100, type = "quartz")
-  plot(db2$date, db2$sal, type = "n", xlab = "Year", ylab = "Salinity (PPT)", ylim = range(db2$sal, na.rm = T), xaxt = "n", main = paste(gages$NWIS_ID[i], "salinity"))
-  axis(1, at = db2$date[which(format(db2$date, "%m") == "01" & format(db2$date, "%d") == "01")], labels = as.numeric(format(Sys.Date(), "%Y")) - 2:0, hadj = -1, padj = 1.7)
-  grid(nx = NA, ny = NULL, col = "black", lty = "dashed")
-  abline(v = db2$date[which(format(db2$date, "%m") == "01" & format(db2$date, "%d") == "01")], lty = "dashed", col = "black")
-  lines(db2$date, db2$sal, lwd = 3)
-  points(db2$date[last_meas], db2$sal[last_meas], pch = 20, cex = 3)
-  lines(db2$date, db3$sal, lwd = 3, col = "green")
-  points(db3$date[last_meas], db3$sal[last_meas], pch = 20, cex = 3, col = "green")
-  text(db3$date[last_meas], db2$sal[last_meas], paste(round(db2$sal[last_meas], 2), "\n", format(db2$date[last_meas], "%m/%d/%Y"), sep = ""), pos = 3, cex = 1.25, font = 2, col = "gray35", offset = 2)
-  text(db3$date[last_meas], db3$sal[last_meas], round(db3$sal[last_meas], 2), pos = 4, cex = 1.25, font = 2, col = "green3", offset = 1)
-  legend("topleft", c(paste(as.numeric(format(Sys.Date(), "%Y")) - 3, "-", format(Sys.Date(), "%Y"), gages$NWIS_ID[i], "salinity"), "Rolling seven-day average salinity"), col = c("black", "green"), lty = 1, lwd = 3)
-  dev.off()
-  err <- try (ftpUpload(paste0("./images/", gages$NWIS_ID[i], "thumb.jpg"), paste0("ftp://ftpint.usgs.gov/pub/er/fl/st.petersburg/eden/coastal_eden_scga/salinity_7day/", gages$NWIS_ID[i], "thumb.jpg"), .opts = list(forbid.reuse = 1)))
-  if (inherits(err, "try-error")) report <- paste0(report, "\n", gages$NWIS_ID[i], " thumbnail NOT transferred") else report <- paste0(report, "\n", gages$NWIS_ID[i], " thumbnail transferred")
-  err <- try (ftpUpload(paste0("./images/", gages$NWIS_ID[i], ".jpg"), paste0("ftp://ftpint.usgs.gov/pub/er/fl/st.petersburg/eden/coastal_eden_scga/salinity_7day/", gages$NWIS_ID[i], ".jpg"), .opts = list(forbid.reuse = 1)))
-  if (inherits(err, "try-error")) report <- paste0(report, "\n", gages$NWIS_ID[i], " full-size NOT transferred") else report <- paste0(report, "\n", gages$NWIS_ID[i], " full-sized transferred")
+  # Create year+ plots of parameters, and thumbnails
+  for (i in 1:dim(gages)[1]) {
+    db2 <- dbGetQuery(con, paste0("select date, ", gages$NWIS_ID[i], "_", j, " as sts from coastal_sc_ga where date >= ", as.numeric(format(Sys.Date(), "%Y")) - 3, format(Sys.Date(), "%m"), "01 order by date"))
+    db2$date <- as.Date(db2$date, format = "%Y-%m-%d")
+    if (any(!is.na(db2$sts))) {
+      last_meas <- rev(which(!is.na(db2$sts)))[1]
+      db3 <- data.frame(date = db2$date, sts = NA)
+      for (k in 7:last_meas) db3$sts[k] <- mean(db2$sts[(k - 6):k], na.rm = T)
+      jpeg(paste0("./images/", gages$NWIS_ID[i], "_", j, "_thumb.jpg"), width = 360, height = 150, pointsize = 2, quality = 100, type = "quartz")
+      par(mar = c(0, 0, 0, 0) + .1)
+      plot(db2$date, db2$sts, type = "n", xlab = "", ylab = "", ylim = range(db2$sts, na.rm = T), xaxt = "n", xaxt = "n", main = "")
+      grid(nx = NA, ny = NULL, col = "black", lty = "dashed")
+      abline(v = which(format(db2$date, "%m") == "01" & format(db2$date, "%d") == "01"), lty = "dashed", col = "black")
+      lines(db2$date, db2$sts, lwd = 2)
+      points(db2$date[last_meas], db2$sts[last_meas], pch = 20)
+      lines(db3$date, db3$sts, lwd = 2, col = "green")
+      points(db3$date[last_meas], db3$sts[last_meas - 6], pch = 20, col = "green")
+      dev.off()
+      jpeg(paste0("./images/", gages$NWIS_ID[i], "_", j, ".jpg"), width = 2400, height = 800, quality = 100, type = "quartz")
+      lab <- if (j == "salinity") "Salinity (PPT)" else if (j == "temperature") "Temperature (°C)" else "Stage (ft.)"
+      plot(db2$date, db2$sts, type = "n", xlab = "Year", ylab = lab, ylim = range(db2$sts, na.rm = T), xaxt = "n", main = paste(gages$NWIS_ID[i], j))
+      axis(1, at = db2$date[which(format(db2$date, "%m") == "01" & format(db2$date, "%d") == "01")], labels = as.numeric(format(Sys.Date(), "%Y")) - 2:0, hadj = -1, padj = 1.7)
+      grid(nx = NA, ny = NULL, col = "black", lty = "dashed")
+      abline(v = db2$date[which(format(db2$date, "%m") == "01" & format(db2$date, "%d") == "01")], lty = "dashed", col = "black")
+      lines(db2$date, db2$sts, lwd = 3)
+      points(db2$date[last_meas], db2$sts[last_meas], pch = 20, cex = 3)
+      lines(db2$date, db3$sts, lwd = 3, col = "green")
+      points(db3$date[last_meas], db3$sts[last_meas], pch = 20, cex = 3, col = "green")
+      text(db3$date[last_meas], db2$sts[last_meas], paste(round(db2$sts[last_meas], 2), "\n", format(db2$date[last_meas], "%m/%d/%Y"), sep = ""), pos = 3, cex = 1.25, font = 2, col = "gray35", offset = 2)
+      text(db3$date[last_meas], db3$sts[last_meas], round(db3$sts[last_meas], 2), pos = 4, cex = 1.25, font = 2, col = "green3", offset = 1)
+      legend("topleft", c(paste(as.numeric(format(Sys.Date(), "%Y")) - 3, "-", format(Sys.Date(), "%Y"), gages$NWIS_ID[i], j), "Rolling seven-day average"), col = c("black", "green"), lty = 1, lwd = 3)
+      dev.off()
+      err <- try (ftpUpload(paste0("./images/", gages$NWIS_ID[i], "_", j, "_thumb.jpg"), paste0("ftp://ftpint.usgs.gov/pub/er/fl/st.petersburg/eden/coastal_eden_scga/", j, "_7day/", gages$NWIS_ID[i], "thumb.jpg"), .opts = list(forbid.reuse = 1)))
+      if (inherits(err, "try-error")) report <- paste0(report, "\n", gages$NWIS_ID[i], j, " thumbnail NOT transferred") else report <- paste0(report, "\n", gages$NWIS_ID[i], j, " thumbnail transferred")
+      err <- try (ftpUpload(paste0("./images/", gages$NWIS_ID[i], "_", j, ".jpg"), paste0("ftp://ftpint.usgs.gov/pub/er/fl/st.petersburg/eden/coastal_eden_scga/", j, "_7day/", gages$NWIS_ID[i], ".jpg"), .opts = list(forbid.reuse = 1)))
+      if (inherits(err, "try-error")) report <- paste0(report, "\n", gages$NWIS_ID[i], j, " full-size NOT transferred") else report <- paste0(report, "\n", gages$NWIS_ID[i], j, " full-sized transferred")
+    }
+  }
 }
 
 query <- "select date_format(date, '%Y') as Year, date_format(date, '%m') as Month"
@@ -166,45 +170,49 @@ err <- try (ftpUpload("./csi/CSI_calculation_data.txt", "ftp://ftpint.usgs.gov/p
 if (inherits(err, "try-error")) report <- paste0(report, "\n", " CSI raw input values NOT transferred") else report <- paste0(report, "\n", " CSI raw input values transferred")
 
 col <- c("tan4", "tan2", "darkolivegreen4", "lightblue", "skyblue3", "darkgreen")
-for (j in 1:length(gages$NWIS_ID)) {
-  por <- dbGetQuery(con, paste0("select date, ", gages$NWIS_ID[j], "_salinity as sal from coastal_sc_ga order by date"))
-  por$date <- as.Date(por$date, format = "%Y-%m-%d")
-  rng <- dbGetQuery(con, paste0("select min(date) as start, max(date) as end from coastal_sc_ga where ", gages$NWIS_ID[j], "_salinity is not null"))
-  yr3 <- dbGetQuery(con, paste0("select date, ", gages$NWIS_ID[j], "_salinity as sal from coastal_sc_ga where date >= ", as.numeric(format(Sys.Date(), "%Y")) - 3, format(Sys.Date(), "%m"), "01 order by date"))
-  yr3$date <- as.Date(yr3$date, format = "%Y-%m-%d")
-  win30 <- rep(NA, length(yr3$date))
-  for (i in 30:length(win30))
-    win30[i] <- mean(yr3$sal[(i-29):i], na.rm = T)
-  qyear <- matrix(NA, length(yr3$date), 6); median <- matrix(NA, length(yr3$date), 1)
-  for (i in 1:length(qyear[, 1])) {
-    qyear[i, ] <- quantile(por$sal[as.POSIXlt(por$date)$mon == as.numeric(format(yr3$date[i], "%m")) - 1], c(0, .1, .25, .75, .9, 1), na.rm = T)
-    median[i] <- quantile(por$sal[as.POSIXlt(por$date)$mon == as.numeric(format(yr3$date[i], "%m")) - 1], 0.5, na.rm = T)
+for (k in c("salinity", "temperature", "stage"))
+  for (j in 1:length(gages$NWIS_ID)) {
+    por <- dbGetQuery(con, paste0("select date, ", gages$NWIS_ID[j], "_", k, " as sts from coastal_sc_ga order by date"))
+    por$date <- as.Date(por$date, format = "%Y-%m-%d")
+    rng <- dbGetQuery(con, paste0("select min(date) as start, max(date) as end from coastal_sc_ga where ", gages$NWIS_ID[j], "_", k, " is not null"))
+    if (!is.na(rng[1])) {
+      yr3 <- dbGetQuery(con, paste0("select date, ", gages$NWIS_ID[j], "_", k, " as sts from coastal_sc_ga where date >= ", as.numeric(format(Sys.Date(), "%Y")) - 3, format(Sys.Date(), "%m"), "01 order by date"))
+      yr3$date <- as.Date(yr3$date, format = "%Y-%m-%d")
+      win30 <- rep(NA, length(yr3$date))
+      for (i in 30:length(win30))
+        win30[i] <- mean(yr3$sts[(i-29):i], na.rm = T)
+      qyear <- matrix(NA, length(yr3$date), 6); median <- matrix(NA, length(yr3$date), 1)
+      for (i in 1:length(qyear[, 1])) {
+        qyear[i, ] <- quantile(por$sts[as.POSIXlt(por$date)$mon == as.numeric(format(yr3$date[i], "%m")) - 1], c(0, .1, .25, .75, .9, 1), na.rm = T)
+        median[i] <- quantile(por$sts[as.POSIXlt(por$date)$mon == as.numeric(format(yr3$date[i], "%m")) - 1], 0.5, na.rm = T)
+      }
+      median[as.POSIXlt(yr3$date)$mday == 1] <- NA
+      jpeg(paste0("./duration_hydrographs/", gages$NWIS_ID[j], "_", k, "_thumb.jpg"), width = 360, height = 150, pointsize = 2, quality = 100, type = "quartz")
+      par(mar = c(0, 0, 0, 0) + .1)
+      plot(qyear[, 1], type = "n", xlab = "", ylab = "", ylim = range(qyear, na.rm = T), xaxt = "n", yaxt = "n", main = "", axes = F, frame.plot = T)
+      for (i in 1:5) polygon(c(1:length(yr3$date), length(yr3$date):1), c(qyear[, i], rev(qyear[, i + 1])), col = col[i])
+      lines(median, lwd = 2, col = "yellow")
+      lines(yr3$sts, lwd = 3, col = "grey")
+      lines(win30, lwd = 4, col = "black")
+      dev.off()
+      jpeg(paste0("./duration_hydrographs/", gages$NWIS_ID[j], "_", k, ".jpg"), width = 2400, height = 800, quality = 100, type = "quartz")
+      par(mar = c(5, 4, 4, 5) + .1)
+      lab <- if (k == "salinity") "Salinity (PPT)" else if (k == "temperature") "Temperature (°C)" else "Stage (ft.)"
+      plot(qyear[, 1], type = "n", xlab = "Month of year", ylab = lab, ylim = range(qyear, na.rm = T), xaxt = "n", main = paste0(gages$NWIS_ID[j], " three year ", k, ", 30 day moving window"))
+      axis(1, at = which(as.POSIXlt(yr3$date)$mday == 1), labels = format(yr3$date[which(as.POSIXlt(yr3$date)$mday == 1)], "%b"), hadj = -1)
+      axis(1, at = which(as.POSIXlt(yr3$date)$yday == 0), labels = format(yr3$date[which(as.POSIXlt(yr3$date)$yday == 0)], "%Y"), padj = 1)
+      for (i in 1:5) polygon(c(1:length(yr3$date), length(yr3$date):1), c(qyear[, i], rev(qyear[, i + 1])), col = col[i])
+      lines(median, lwd = 3, col = "yellow")
+      lines(yr3$sts, lwd = 4, col = "grey")
+      lines(win30, lwd = 5, col = "black")
+      legend("topright", c("90% to Max.", "75% to 90%", "25% to 75%", "10% to 25%", "Min. to 10%"), fill = col[5:1], cex = 1.5, bty = "n", title = paste(k, "bins"))
+      legend("topright", c(paste(k, "30 day moving window"), paste("Daily", k, "values"), paste("Historic monthly mean", k)), lwd = c(5, 4, 4), col = c("black", "grey", "yellow"), inset = c(.075, 0), cex = 1.5, bty = "n")
+      legend("topleft", paste("Period of record:", rng$start, "to", rng$end), cex = 1.5, bty = "n")
+      dev.off()
+      ftpUpload(paste0("./duration_hydrographs/", gages$NWIS_ID[j], "_", k, "_thumb.jpg"), paste0("ftp://ftpint.usgs.gov/pub/er/fl/st.petersburg/eden/coastal_eden_scga/", k, "_30day/", gages$NWIS_ID[j], "thumb.jpg"), .opts = list(forbid.reuse = 1))
+      ftpUpload(paste0("./duration_hydrographs/", gages$NWIS_ID[j], "_", k, ".jpg"), paste0("ftp://ftpint.usgs.gov/pub/er/fl/st.petersburg/eden/coastal_eden_scga/", k, "_30day/", gages$NWIS_ID[j], ".jpg"), .opts = list(forbid.reuse = 1))
+    }
   }
-  median[as.POSIXlt(yr3$date)$mday == 1] <- NA
-  jpeg(paste0("./duration_hydrographs/", gages$NWIS_ID[j], "thumb.jpg"), width = 360, height = 150, pointsize = 2, quality = 100, type = "quartz")
-  par(mar = c(0, 0, 0, 0) + .1)
-  plot(qyear[, 1], type = "n", xlab = "", ylab = "", ylim = range(qyear, na.rm = T), xaxt = "n", yaxt = "n", main = "", axes = F, frame.plot = T)
-  for (i in 1:5) polygon(c(1:length(yr3$date), length(yr3$date):1), c(qyear[, i], rev(qyear[, i + 1])), col = col[i])
-  lines(median, lwd = 2, col = "yellow")
-  lines(yr3$sal, lwd = 3, col = "grey")
-  lines(win30, lwd = 4, col = "black")
-  dev.off()
-  jpeg(paste0("./duration_hydrographs/", gages$NWIS_ID[j], ".jpg"), width = 2400, height = 800, quality = 100, type = "quartz")
-  par(mar = c(5, 4, 4, 5) + .1)
-  plot(qyear[, 1], type = "n", xlab = "Month of year", ylab = "Salinity (PPT)", ylim = range(qyear, na.rm = T), xaxt = "n", main = paste(gages$NWIS_ID[j], "three year salinity, 30 day moving window"))
-  axis(1, at = which(as.POSIXlt(yr3$date)$mday == 1), labels = format(yr3$date[which(as.POSIXlt(yr3$date)$mday == 1)], "%b"), hadj = -1)
-  axis(1, at = which(as.POSIXlt(yr3$date)$yday == 0), labels = format(yr3$date[which(as.POSIXlt(yr3$date)$yday == 0)], "%Y"), padj = 1)
-  for (i in 1:5) polygon(c(1:length(yr3$date), length(yr3$date):1), c(qyear[, i], rev(qyear[, i + 1])), col = col[i])
-  lines(median, lwd = 3, col = "yellow")
-  lines(yr3$sal, lwd = 4, col = "grey")
-  lines(win30, lwd = 5, col = "black")
-  legend("topright", c("90% to Max.", "75% to 90%", "25% to 75%", "10% to 25%", "Min. to 10%"), fill = col[5:1], cex = 1.5, bty = "n", title = "Salinity bins")
-  legend("topright", c("Salinity 30 day moving window", "Daily salinity values", "Historic monthly mean salinity"), lwd = c(5, 4, 4), col = c("black", "grey", "yellow"), inset = c(.075, 0), cex = 1.5, bty = "n")
-  legend("topleft", paste("Period of record:", rng$start, "to", rng$end), cex = 1.5, bty = "n")
-  dev.off()
-  ftpUpload(paste0("./duration_hydrographs/", gages$NWIS_ID[j], "thumb.jpg"), paste0("ftp://ftpint.usgs.gov/pub/er/fl/st.petersburg/eden/coastal_eden_scga/salinity_30day/", gages$NWIS_ID[j], "thumb.jpg"), .opts = list(forbid.reuse = 1))
-  ftpUpload(paste0("./duration_hydrographs/", gages$NWIS_ID[j], ".jpg"), paste0("ftp://ftpint.usgs.gov/pub/er/fl/st.petersburg/eden/coastal_eden_scga/salinity_30day/", gages$NWIS_ID[j], ".jpg"), .opts = list(forbid.reuse = 1))
-}
 
 query <- "select date"
 for (j in 1:dim(gages)[1])
